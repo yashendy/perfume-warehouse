6b4cd38bc6c9b7011b2f100eaf789c17f5b637f8
sunrise <yashendy@gmail.com>
2025-08-30 19:16:01 +0300

    Create index.js

---
+++ web/screens/index.js
@@ -0,0 +1,8 @@
+export { dashboard } from './dashboard.js';
+export { products } from './products.js';
+export { bulk_lots } from './bulk_lots.js';
+export { decant_jobs } from './decant_jobs.js';
+export { items } from './items.js';
+export { dispatch } from './dispatch.js';
+export { settlements } from './settlements.js';
+export { monthly_inventory } from './monthly_inventory.js';
